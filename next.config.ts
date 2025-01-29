a8976b1acb02e9f84a7645322868efa6adffaac5
punkestu <pangestubima89@gmail.com>
2025-01-29 19:15:00 +0700

    add samehadaku domain to images next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["samehadaku.mba"],
+  }
 };
 
 export default nextConfig;
