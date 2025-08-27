6fb1849d756c5fb126cbde6a2404ef82ea400f1f
punkestu <pangestubima89@gmail.com>
2025-08-27 22:50:24 +0700

    feat: implement logout functionality with API route and update profile link

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,7 @@ export default function ProfilePage() {
     <main className="flex flex-col min-h-[70vh] p-4 items-center">
       <h1 className="my-2 text-4xl font-bold dark:text-white text-center flex gap-8">
         Profile Page
-        <Link href={"/api/logout"}
+        <Link href="/logout" prefetch={false}
           className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none w-fit"
         >
           Logout
